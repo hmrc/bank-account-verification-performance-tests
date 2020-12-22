3c19ee92fb3ae3039038180c7aa369dd665c06b4
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-12-22 09:08:12 +0000

    Update dependencies to latest versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "com.typesafe" % "config" % "1.4.0" % Test,
-    "uk.gov.hmrc" %% "performance-test-runner" % "3.7.0" % Test,
+    "com.typesafe" % "config" % "1.4.1" % Test,
+    "uk.gov.hmrc" %% "performance-test-runner" % "3.10.0" % Test,
     "io.gatling" % "gatling-test-framework" % "2.3.1" % Test,
     "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.3.1" % Test
   )
