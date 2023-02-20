2d8a4902db27322469410935ef385113437cf347
berlin-hmrc <berlin.bernadin1@digital.hmrc.gov.uk>
2023-02-20 15:33:49 +0000

    Scala and Copyright version update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe" % "config" % "1.4.1" % Test,
-    "uk.gov.hmrc" %% "performance-test-runner" % "5.+" % Test,
-    "io.gatling" % "gatling-test-framework" % "3.4.2" % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % "3.4.2" % Test
+    "uk.gov.hmrc" %% "performance-test-runner" % "5.4.0" % Test,
+    "io.gatling" % "gatling-test-framework" % "3.5.1" % Test,
+    "io.gatling.highcharts" % "gatling-charts-highcharts" % "3.5.1" % Test
   )
 }
