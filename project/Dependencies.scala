8804ba68ab9f772f0cf8e6d2a247aa110cd5af7c
usmn-m <125829801+usmn-m@users.noreply.github.com>
2025-07-10 16:07:40 +0100

    dependency uplift

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,6 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe" % "config" % "1.4.1" % Test,
-    "uk.gov.hmrc" %% "performance-test-runner" % "5.5.0" % Test,
-    "io.gatling" % "gatling-test-framework" % "3.5.1" % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % "3.5.1" % Test
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0" % Test
   )
 }
