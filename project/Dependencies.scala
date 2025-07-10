8e6859c5b1d673432a4820326fc56493d9cbf075
usmn-m <125829801+usmn-m@users.noreply.github.com>
2025-07-10 16:48:37 +0100

    added play-json to Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe" % "config" % "1.4.1" % Test,
+    "com.typesafe.play" %% "play-json" % "2.10.7" % Test,
     "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0" % Test
   )
 }
