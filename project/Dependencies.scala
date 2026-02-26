b99f6cceb3ee60181ecac199f64a294c8a1b7f68
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2026-02-26 12:01:13 +0000

    [CIR-2767] Lower JPS in line with production (post COVID traffic) (#6)
    * [CIR-2767] Lower JPS in line with production (post COVID traffic)
    
    * [CIR-2767] (chore) dep bumps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "org.playframework" %% "play-json"                % "3.0.5",
-    "uk.gov.hmrc"       %% "performance-test-runner"  % "6.2.0"
+    "org.playframework" %% "play-json"                % "3.0.6",
+    "uk.gov.hmrc"       %% "performance-test-runner"  % "6.3.0"
   ).map(_ % Test)
 }
